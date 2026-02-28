552407848cd2de038523311f02e0b384c6d82e07
Vishnu G <vishnugkannath@gmail.com>
2026-02-28 14:06:22 +0530

    chore: iniy React Practice Hub with Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>React Practice Hub</h1>
+      <p>Getting started with React + Vite.</p>
+    </div>
+  );
+}
+
+export default App
